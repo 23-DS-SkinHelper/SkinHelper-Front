4c0c269d8ebfd7ef8c12c27a22903602ebccb458
heehminh <mink141416@gmail.com>
2023-10-26 14:59:35 +0900

    chore: tailwindcss, storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,19 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
     "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: ["**/stories/**"],
+        optionalDependencies: false,
+      },
+    ],
   },
 };
