f501ac6bf9844476fcb321f32ab6837fd99b5a60
heehminh <mink141416@gmail.com>
2023-10-26 17:31:05 +0900

    feat: homeScreen UI

---
+++ src/components/atoms/Header/Title.tsx
@@ -0,0 +1,10 @@
+const Title = () => {
+  return (
+    <div className="flex flex-col items-end leading-32 text-28 font-semibold">
+      <div>AI Image Analysis</div>
+      <div>for Early Skin Disease Detection</div>
+    </div>
+  );
+};
+
+export default Title;
